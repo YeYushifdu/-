6f677a8f5f81cbc08570af11a43641a4811d2d84
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-13 23:53:21 +0800

    Add files via upload

---
+++ 函数.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+//int main() {
+//	char arr1[20] = { 0 };
+//	char arr2[] = "abc";
+//	strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+//函数的组成
+//ret_type fun_name(para1, *) {
+//	statement;    //语句项
+//}
+//ret_type  返回类型
+//fun_name  函数名
+//para1  函数参数
+
