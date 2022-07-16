b64a1134645ed60141ec3a779f066778e91a2577
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-16 16:46:14 +0800

    Add files via upload

---
+++ 比特c语言/打印闰年.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main() {
+//	int i = 1000;
+//	printf("闰年年份为：");
+//	for (i = 1000; i <= 2000; i++) {
+//		if (i % 4 == 0) {
+//			if (i % 100 != 0) {
+//				printf("%d ", i);
+//			}
+//			else {
+//				if (i % 400 == 0) {
+//					printf("%d ", i);
+//				}
+//			}
+//		}
+//	}
+//	return 0;
+//}
+
+int main() {
+	int y = 0, count = 0;
+	for (y = 1000; y <= 2000; y++) {
+		//判断y是不是闰年
+		//1.被4整除且不能被100整除是闰年
+		//2.能被400整除是闰年
+		//if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0)) 
+		if (y % 4 == 0) {
+			if (y % 100 != 0) {
+				printf("%d ", y);
+				count++;
+			}
+		}
+		if (y % 400 == 0) {
+			printf("%d ", y);
+			count++;
+		}
+	}
+	printf("\ncount=%d", count);
+	return 0;
+}
