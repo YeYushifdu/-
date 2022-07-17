f58eaf527618478c3c2df8137fcafde16952484b
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-17 23:39:17 +0800

    Add files via upload

--- 比特c语言/打印素数.c
+++ 比特c语言/打印素数.c
@@ -37,3 +37,19 @@
 //	return 0;
 //}
 
+int main() {
+	int i = 0, j = 0;
+	for (i = 100; i <= 200; i++) {
+		int flag = 1;
+		for (j = 2; j < i; j++) {
+			if (i % j == 0) {
+				flag = 0;    //²»ÊÇËØÊı
+				break;
+			}
+		}
+		if (flag == 1) {
+			printf("%d ", i);
+		}
+	}
+	return 0;
+}
