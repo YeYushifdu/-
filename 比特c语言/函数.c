deae7d81ed6dacdbcefe454ad5388279367fc6c9
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-19 20:59:56 +0800

    Add files via upload

--- 比特c语言/函数.c
+++ 比特c语言/函数.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<string.h>
 
+//strcpy()函数
 //int main() {
 //	char arr1[20] = { 0 };
 //	char arr2[] = "abc";
@@ -9,6 +10,19 @@
 //	printf("%s\n", arr1);
 //	return 0;
 //}
+
+
+//memset()函数--内存设置
+//把arr中前5个字符设为'x'
+int main() {
+	char arr[] = "hello world";
+	memset(arr, 'x', 5);
+	printf("%s\n", arr);
+	return 0;
+}
+
+
+
 
 //函数的组成
 //ret_type fun_name(para1, *) {
