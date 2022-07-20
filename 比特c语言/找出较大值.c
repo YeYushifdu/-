81fb32472869ba5b5e2f3b5193d0668ec120fc25
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-20 21:13:51 +0800

    Add files via upload

--- 比特c语言/找出较大值.c
+++ 比特c语言/找出较大值.c
@@ -1,18 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 //找出两个整数中的较大值
-int main() {
-	int a = 0, b = 0;
-	printf("请输入两个整数：");
-	scanf("%d %d", &a, &b);
-	if (a > b) {
-		printf("较大值为%d\n", a);
-	}
-	else if (b > a) {
-		printf("较大值为%d\n", b);
+//int main() {
+//	int a = 0, b = 0;
+//	printf("请输入两个整数：");
+//	scanf("%d %d", &a, &b);
+//	if (a > b) {
+//		printf("较大值为%d\n", a);
+//	}
+//	else if (b > a) {
+//		printf("较大值为%d\n", b);
+//	}
+//	else {
+//		printf("两数一样大\n");
+//	}
+//	return 0;
+//}
+
+get_max(int x, int y) {
+	int z = 0;
+	if (x > y) {
+		z = x;
 	}
 	else {
-		printf("两数一样大\n");
+		z = y;
 	}
+	return z;
+}
+
+int main() {
+	int a = 10, b = 20;
+	int max = get_max(a, b);
+	printf("max=%d\n", max);
 	return 0;
 }
