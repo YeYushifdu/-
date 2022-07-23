775b2ce7dfdd392ca21e22ae11dfa599e6ed71f2
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-23 23:06:58 +0800

    Add files via upload

--- 比特c语言/打印素数.c
+++ 比特c语言/打印素数.c
@@ -55,20 +55,42 @@
 //	return 0;
 //}
 
-//考虑到：
-//若m=a*b，则a和b中至少有一个数字小于等于根号m
-//即因子只需判断至根号m
-int main() {
-	int i = 0, j = 0;
-	for (i = 100; i <= 200; i++) {
-		int flag = 1;
-		for (j = 2; j <= sqrt(i); j++) {
-			if (i % j == 0) {
-				flag = 0;    //不是素数
-				break;
-			}
+////考虑到：
+////若m=a*b，则a和b中至少有一个数字小于等于根号m
+////即因子只需判断至根号m
+//int main() {
+//	int i = 0, j = 0;
+//	for (i = 100; i <= 200; i++) {
+//		int flag = 1;
+//		for (j = 2; j <= sqrt(i); j++) {
+//			if (i % j == 0) {
+//				flag = 0;    //不是素数
+//				break;
+//			}
+//		}
+//		if (flag == 1) {
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//调用函数实现
+int is_prime(int n) {
+	int j = 0;
+	for (j = 2; j < n; j++) {
+		if (n % j == 0) {
+			return 0;
 		}
-		if (flag == 1) {
+	}
+	return 1;
+}
+
+int main() {
+	int i = 0;
+	for (i = 100; i <= 200; i++) {
+		//判断i是否为素数
+		if (is_prime(i) == 1) {
 			printf("%d ", i);
 		}
 	}
