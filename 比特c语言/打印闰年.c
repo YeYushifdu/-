3373469bd7fbcb0db81628b487185c80fd5be85f
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-23 23:09:22 +0800

    Add files via upload

--- 比特c语言/打印闰年.c
+++ 比特c语言/打印闰年.c
@@ -18,24 +18,41 @@
 //	return 0;
 //}
 
+//int main() {
+//	int y = 0, count = 0;
+//	for (y = 1000; y <= 2000; y++) {
+//		//判断y是不是闰年
+//		//1.被4整除且不能被100整除是闰年
+//		//2.能被400整除是闰年
+//		//if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0)) 
+//		if (y % 4 == 0) {
+//			if (y % 100 != 0) {
+//				printf("%d ", y);
+//				count++;
+//			}
+//		}
+//		if (y % 400 == 0) {
+//			printf("%d ", y);
+//			count++;
+//		}
+//	}
+//	printf("\ncount=%d", count);
+//	return 0;
+//}
+
+//调用函数实现
+int is_leap_year(int n) {    //如果判断是闰年返回1
+	if ((n % 4 == 0) && (n % 100 != 0) || (n % 400 == 0)) {
+		return 1;
+	}
+	return 0;    //不是闰年返回0
+}
 int main() {
-	int y = 0, count = 0;
+	int y = 0;
 	for (y = 1000; y <= 2000; y++) {
-		//判断y是不是闰年
-		//1.被4整除且不能被100整除是闰年
-		//2.能被400整除是闰年
-		//if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0)) 
-		if (y % 4 == 0) {
-			if (y % 100 != 0) {
-				printf("%d ", y);
-				count++;
-			}
-		}
-		if (y % 400 == 0) {
+		if (is_leap_year(y) == 1) {
 			printf("%d ", y);
-			count++;
 		}
 	}
-	printf("\ncount=%d", count);
 	return 0;
 }
