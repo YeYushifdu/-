832407d837711f5839cc5ea6ca9c12265dec0015
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-24 18:08:26 +0800

    Add files via upload

--- 比特c语言/整型有序二分查找.c
+++ 比特c语言/整型有序二分查找.c
@@ -35,6 +35,35 @@
 //int binsearch(int x, int v[], int n); 
 //	int x = 0;
 
+//调用函数实现
+int binary_search(int a[], int k, int s) {
+	int left = 0, right = s - 1;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (a[mid] > k) {
+			right = mid - 1;
+		}
+		else if (a[mid] < k) {
+			left = mid + 1;
+		}
+		else {
+			return mid;
+		}
+	}
+	return -1;    //找不到
+}
 int main() {
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int key = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//找到了就返回找到的位置的下标
+	//找不到就返回-1
+	int ret = binary_search(arr, key, sz);
+	if (-1 == ret) {
+		printf("找不到！\n");
+	}
+	else {
+		printf("找到了，下标是%d\n", ret);
+	}
 	return 0;
 }
