d12ef2d4ddc9e761b03988e98cabc7d2f2600e42
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-25 18:19:36 +0800

    Add files via upload

---
+++ 比特c语言/函数的链式访问.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+int main() {
+	//int len = strlen("abc");
+	//printf("%d\n", len);
+	////链式访问
+	//printf("%d\n", strlen("abc"));
+	//char arr1[20] = { 0 };
+	//char arr2[] = "hello";
+	//printf("%s\n", strcpy(arr1, arr2));
+	printf("%d", printf("%d", printf("%d", 43)));
+	//printf()函数返回值是打印在屏幕上字符的个数
+	//最内层printf("%d", 43)打印"43"，返回值2
+	//第二层打印"2"，返回值1
+	//最外层打印"1"
+	return 0;
+}
