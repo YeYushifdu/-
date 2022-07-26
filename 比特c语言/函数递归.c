eff1e0eeed1affa4e03b97898a1206762e35d95a
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-26 15:41:53 +0800

    Add files via upload

--- 比特c语言/函数递归.c
+++ 比特c语言/函数递归.c
@@ -9,15 +9,27 @@
 //按顺序打印整型的每一位
 //输入：1234
 //输出：1 2 3 4
-void print(unsigned int n) {
-	if (n > 9) {
-		print(n / 10);
+//void print(unsigned int n) {
+//	if (n > 9) {
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+//int main() {
+//	unsigned int num = 0;
+//	scanf("%u", &num);    //1234
+//	print(num);    //print()函数可以打印参数部分数字的每一位
+//	return 0;
+//}
+
+//栈溢出
+void test(int n) {
+	if (n < 10000) {
+		test(n + 1);
 	}
-	printf("%d ", n % 10);
 }
+
 int main() {
-	unsigned int num = 0;
-	scanf("%u", &num);    //1234
-	print(num);    //print()函数可以打印参数部分数字的每一位
+	test(1);
 	return 0;
 }
