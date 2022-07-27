8768c90a9b025ec8dba404b63949f51454dffc2c
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-27 18:46:58 +0800

    Add files via upload

--- 比特c语言/计算n的阶乘.c
+++ 比特c语言/计算n的阶乘.c
@@ -2,16 +2,32 @@
 #include<stdio.h>
 //计算n的阶乘
 
+//int main() {
+//	int n = 0;
+//	int i = 1;
+//	int mul = 1;
+//	printf("输入整数n：");
+//	scanf("%d", &n);
+//	for (i = 1; i <=n; i++) {
+//		mul *= i;
+//	}
+//	printf("n的阶乘为:%d\n", mul);
+//	return 0;
+//}
+
+//函数递归实现
+int Fac(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	else {
+		return n * Fac(n - 1);
+	}
+}
 int main() {
 	int n = 0;
-	int i = 1;
-	int mul = 1;
-	printf("输入整数n：");
 	scanf("%d", &n);
-	for (i = 1; i <=n; i++) {
-		mul *= i;
-	}
-	printf("n的阶乘为:%d\n", mul);
+	int ret = Fac(n);
+	printf("%d\n", ret);
 	return 0;
 }
-
