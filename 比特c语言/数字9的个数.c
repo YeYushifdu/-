72082a1f47cb51441aad5f0409d9188a5765f2dc
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-30 17:51:37 +0800

    Add files via upload

---
+++ 比特c语言/数字9的个数.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int i = 0, count = 0;
+	for (i = 1; i <= 100; i++) {
+		if (i % 10 == 9) {
+			count++;
+		}
+		if (i / 10 == 9) {
+			count++;
+		}
+	}
+	printf("1-100中9的个数为：%d\n", count);
+	return 0;
+}
