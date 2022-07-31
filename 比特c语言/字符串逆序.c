0efdb35dd1cfde273bbc8f71d5696a81e9767aef
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-07-31 21:29:01 +0800

    Add files via upload

--- 比特c语言/字符串逆序.c
+++ 比特c语言/字符串逆序.c
@@ -1,6 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 //字符串逆序
+//把第一位与最后一位交换
+int my_strlen(char* str) {
+	int count = 0;
+	while (*str != '0') {
+		count++;
+		str++;
+	}
+	return count;
+}
+void reverse_string(char* str) {
+	int left = 0;
+	int right = my_strlen(str) - 1;
+	while (left < right) {
+		char tmp = str[left];
+		str[left] = str[right];
+		str[right] = tmp;
+		left++;
+		right--;
+	}
+}
 int main() {
 	char arr[] = "abcdef";
 	reverse_string(arr);    //数组名arr是数组arr首元素的地址
