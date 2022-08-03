094965e1321c9466ad6784a5b771750710399433
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-08-03 21:21:50 +0800

    Add files via upload

--- 比特c语言/字符串逆序.c
+++ 比特c语言/字符串逆序.c
@@ -4,22 +4,32 @@
 //把第一位与最后一位交换
 int my_strlen(char* str) {
 	int count = 0;
-	while (*str != '0') {
+	while (*str != '\0') {
 		count++;
 		str++;
 	}
 	return count;
 }
+//void reverse_string(char* str) {
+//	int left = 0;
+//	int right = my_strlen(str) - 1;
+//	while (left < right) {
+//		char tmp = str[left];
+//		str[left] = str[right];
+//		str[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
 void reverse_string(char* str) {
-	int left = 0;
-	int right = my_strlen(str) - 1;
-	while (left < right) {
-		char tmp = str[left];
-		str[left] = str[right];
-		str[right] = tmp;
-		left++;
-		right--;
+	char tmp = *str;
+	int len = my_strlen(str);
+	*str = *(str + len - 1);
+	*(str + len - 1) = '\0';
+	if (my_strlen(str + 1) >= 2) {
+		reverse_string(str + 1);
 	}
+	*(str + len - 1) = tmp;
 }
 int main() {
 	char arr[] = "abcdef";
@@ -27,3 +37,4 @@ int main() {
 	printf("%s\n", arr);    //fedcba
 	return 0;
 }
+
