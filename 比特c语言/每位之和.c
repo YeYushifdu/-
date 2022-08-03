aef0fb66bc1c3810eb7538e9ac8f162e54ae8193
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-08-03 21:32:46 +0800

    Add files via upload

---
+++ 比特c语言/每位之和.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//计算一个数的每位之和
+int sum(int n) {
+	int num = 0;
+	while (n >= 10) {
+		num += n % 10;
+		n = (n - n % 10) / 10;
+	}
+	num += n;
+	return num;
+}
+int main() {
+	int n = 0, num = 0;
+	printf("请输入一个整数：");
+	scanf("%d", &n);
+	num = sum(n);
+	printf("该数每位之和为：%d\n", num);
+	return 0;
+}
