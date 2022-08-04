f007914a2d82ed8f21b86612b88a7f415fcf2616
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-08-04 23:34:57 +0800

    Add files via upload

--- 比特c语言/每位之和.c
+++ 比特c语言/每位之和.c
@@ -1,14 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 //计算一个数的每位之和
+//int sum(int n) {
+//	int num = 0;
+//	while (n >= 10) {
+//		num += n % 10;
+//		n = (n - n % 10) / 10;
+//	}
+//	num += n;
+//	return num;
+//}
+//函数递归实现
 int sum(int n) {
-	int num = 0;
-	while (n >= 10) {
-		num += n % 10;
-		n = (n - n % 10) / 10;
+	if (n >= 10) {
+		return sum(n / 10) + (n % 10);
 	}
-	num += n;
-	return num;
+	else {
+		return n;
+	}
 }
 int main() {
 	int n = 0, num = 0;
