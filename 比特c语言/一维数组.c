4981c228e370d45e689fdb950fc6acfd5cece2cf
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-08-06 23:30:53 +0800

    Add files via upload

--- 比特c语言/一维数组.c
+++ 比特c语言/一维数组.c
@@ -1,7 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+//int main() {
+//	//int n = 8;
+//	//int arr[n];
+//	//return 0;
+//	int arr[10] = { 0 };	//初始化第一项为0，其它项默认为0
+//	arr[4] = 5;		//[]下标引用操作符
+//}
+//存储位置
+//%p专门用来打印地址(十六进制)
 int main() {
-	int n = 8;
-	int arr[n];
+	int arr[10] = { 0 };
+	int i = 0;
+	for (i = 0; i < 10; i++) {
+		printf("&arr[%d]=%p\n", i, &arr[i]);
+	}
 	return 0;
 }
