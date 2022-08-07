eda8a50a645ac5bca7b7c62784cd37c5ec5aa84b
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-08-07 22:36:35 +0800

    Add files via upload

--- 比特c语言/一维数组.c
+++ 比特c语言/一维数组.c
@@ -10,10 +10,17 @@
 //存储位置
 //%p专门用来打印地址(十六进制)
 int main() {
-	int arr[10] = { 0 };
+	//int arr[10] = { 0 };
+	//int i = 0;
+	//for (i = 0; i < 10; i++) {
+	//	printf("&arr[%d]=%p\n", i, &arr[i]);
+	//}
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int* p = arr;	//数组名是数组首元素的地址
 	int i = 0;
 	for (i = 0; i < 10; i++) {
-		printf("&arr[%d]=%p\n", i, &arr[i]);
+		printf("%d ", *p);
+		p++;
 	}
 	return 0;
 }
