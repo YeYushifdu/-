d13218860b45cd3bd9623a7a5e523291abdc177f
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-08-10 23:31:12 +0800

    Add files via upload

--- 三子棋/game.h
+++ 三子棋/game.h
@@ -1,1 +1,9 @@
 #pragma once
+//符号的定义
+#define ROW 3
+#define COL 3
+//函数的声明
+//初始化棋盘
+void Intboard(char board[ROW][COL], int row, int col);
+//打印棋盘
+void Displayboard(char board[ROW][COL], int row, int col);
