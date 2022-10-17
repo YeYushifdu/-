a080f442261987e213329280e58d0862937f8057
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-10-18 00:17:28 +0800

    Add files via upload

--- 比特c语言/调试实例.c
+++ 比特c语言/调试实例.c
@@ -35,12 +35,49 @@
 //}
 
 //实例2
-int main() {
-	int i = 0;
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	for (i = 0; i <= 12; i++) {
-		arr[i] = 0;
-		printf("a");
+//int main() {
+//	int i = 0;
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++) {
+//		arr[i] = 0;
+//		printf("a");
+//	}
+//	return 0;
+//}
+
+//实例3
+//输入一个整数数组，实现函数
+//调整该数组中的数组的顺序
+//使得数组中所有的奇数位于数组的前半部分，偶数位于后半部分
+void move(int arr[], int sz) {
+	//从前往后找偶数，从后往前找奇数，然后交换
+	int left = 0;
+	int right = sz - 1;
+	while (left < right) {
+		while ((left < right) && (arr[left] % 2 == 1)) {
+			left++;
+		}
+		while ((left < right) && (arr[right] % 2 == 0)) {
+			right--;
+		}
+		if (left < right) {
+			int tmp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = tmp;
+		}
 	}
+}
+void print(int arr[], int sz) {
+	int i = 0;
+	for (i = 0; i < sz; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+int main() {
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	move(arr, sz);
+	print(arr, sz);
 	return 0;
 }
