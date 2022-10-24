6fe4087466c16cbb218721f441e45b5e44a83f76
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-10-24 22:57:06 +0800

    Add files via upload

--- 比特c语言/指针进阶.c
+++ 比特c语言/指针进阶.c
@@ -325,3 +325,57 @@
 //	} while (input);
 //	return 0;
 //}
+
+//冒泡排序
+//void bubble_sort(int arr[], int sz) {
+//	int i = 0;						//冒泡排序地趟数
+//	for (i = 0; i < sz - 1; i++) {	//一趟冒泡排序
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++) {
+//			if (arr[j] > arr[j + 1]) {
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
+//void print_arr(int arr[], int sz) {
+//	int i = 0;
+//	for (i = 0; i < sz; i++) {
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//int main() {						//升序
+//	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//	print_arr(arr, sz);
+//	return 0;
+//}
+
+
+//快速排序
+void qsort(void* base,				//base中存放待排序数据中第一个对象地地址
+	size_t num,						//排序数据元素的个数
+	size_t size,					//排序数据中一个元素的大小，单位是字节
+	int(*cmp)(const void*, const void*)	//比较待排序数据中两个元素的函数
+);
+int cmp_int(const void* e1, const void* e2) {
+	return (*(int*)e1 - *(int*)e2);
+}
+void print_arr(int arr[], int sz) {
+	int i = 0;
+	for (i = 0; i < sz; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+int main() {						//升序
+	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+	print_arr(arr, sz);
+	return 0;
+}
