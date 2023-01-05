cae186946c3aad05e06347aba1f33b882021a8c6
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2023-01-05 15:50:26 +0800

    Add files via upload

--- 比特c语言/指针进阶2.c
+++ 比特c语言/指针进阶2.c
@@ -24,14 +24,52 @@ int main() {
 	//printf("%d\n", sizeof(&arr + 1));	//数组后面的空间的地址的大小
 	//printf("%d\n", sizeof(&arr[0] + 1));//第二个元素地址的大小
 
-	char arr[] = { 'a','b','c','d','e','f' };
-	printf("%d\n", strlen(arr));		//数组大小	
-	printf("%d\n", strlen(arr + 0));	//第一个元素地址的大小
-	printf("%d\n", strlen(*arr));		//第一个元素（字符）的大小
-	printf("%d\n", strlen(arr[1]));		//第二个元素（字符）的大小
-	printf("%d\n", strlen(&arr));		//地址的大小
-	printf("%d\n", strlen(&arr + 1));	//数组后面的空间的地址的大小
-	printf("%d\n", strlen(&arr[0] + 1));//第二个元素地址的大小
+	//char arr[] = { 'a','b','c','d','e','f' };
+	//printf("%d\n", strlen(arr));		//找'\0'，随机值	
+	//printf("%d\n", strlen(arr + 0));	//随机值
+	//printf("%d\n", strlen(*arr));		//把a(97)当作地址解引用，出错
+	//printf("%d\n", strlen(arr[1]));		//把b（98）当作地址，出错
+	//printf("%d\n", strlen(&arr));		//随机值
+	//printf("%d\n", strlen(&arr + 1));	//数组后面的空间找'\0'，随机值-6
+	//printf("%d\n", strlen(&arr[0] + 1));//第二个元素开始找'\0'，随机值-1
+
+	//char arr[] = "abcdef";
+	//printf("%d\n", sizeof(arr));		//数组大小，7	
+	//printf("%d\n", sizeof(arr + 0));	//第一个元素地址的大小
+	//printf("%d\n", sizeof(*arr));		//第一个元素（字符）的大小
+	//printf("%d\n", sizeof(arr[1]));		//第二个元素（字符）的大小
+	//printf("%d\n", sizeof(&arr));		//地址的大小
+	//printf("%d\n", sizeof(&arr + 1));	//数组后面的空间的地址的大小
+	//printf("%d\n", sizeof(&arr[0] + 1));//第二个元素地址的大小
+
+	//char arr[] = "abcdef";
+	//printf("%d\n", strlen(arr));		//数组长度，6	
+	//printf("%d\n", strlen(arr + 0));	//同上
+	//printf("%d\n", strlen(*arr));		//把a(97)当作地址解引用，出错
+	//printf("%d\n", strlen(arr[1]));		//把b（98）当作地址，出错
+	//printf("%d\n", strlen(&arr));		//6
+	//printf("%d\n", strlen(&arr + 1));	//数组后面的空间找'\0'，随机值
+	//printf("%d\n", strlen(&arr[0] + 1));//第二个元素开始找'\0'，5
+
+	//char* p = "abcdef";
+	//printf("%d\n", sizeof(p));		//指针变量大小
+	//printf("%d\n", sizeof(p + 1));	//第二个元素指针变量的大小
+	//printf("%d\n", sizeof(*p));		//第一个元素（字符）的大小
+	//printf("%d\n", sizeof(p[0]));		//第一个元素（字符）的大小
+	//printf("%d\n", sizeof(&p));		//地址的大小
+	//printf("%d\n", sizeof(&p + 1));	//地址的大小
+	//printf("%d\n", sizeof(&p[0] + 1));//第二个元素地址的大小
+
+	char* p = "abcdef";
+	printf("%d\n", strlen(p));		//数组长度，6
+	printf("%d\n", strlen(p + 1));	//5
+	printf("%d\n", strlen(*p));		//第一个元素（ASCII码），出错
+	printf("%d\n", strlen(p[0]));	//同上
+	printf("%d\n", strlen(&p));		//从p的位置开始找'\0'，随机值
+	printf("%d\n", strlen(&p + 1));	//从p的位置加4（或8）开始找'\0'，随机值
+	printf("%d\n", strlen(&p[0] + 1));//5
+	//二维数组
+
 	return 0;
 }
 
