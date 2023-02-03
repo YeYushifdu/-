f5c7f2c21e4b0edeb5b31999f070ae94ce0f825c
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2023-02-03 23:33:05 +0800

    Add files via upload

--- 比特c语言/字符串函数.c
+++ 比特c语言/字符串函数.c
@@ -1,9 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<string.h>
+//#include<string.h>
+//int my_strlen(const char* str) {
+//	int count = 0;	//计数器
+//	assert(str != NULL);
+//	while (*str != '\0') {
+//		count++; 
+//		str++;		//移向下一个字符
+//	}
+//	return count;
+//}
+//int main() {
+//	char arr[] = "abc";
+//	//int len = strlen(arr);
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
 int main() {
-	char arr[] = "abc";
-	int len = strlen(arr);
-	printf("%d\n", len);
+	if (strlen("abc") - strlen("abcdef") > 0) {
+		printf(">\n");
+	}
+	else {
+		printf("<=\n");
+	}
 	return 0;
 }
